1e7a044ddd7bbb20a80157ce3a0eb547ba52bc89
Tomsztyl <60610578+Tomsztyl@users.noreply.github.com>
2021-01-12 00:08:12 +0100

    Add files via upload

---
+++ UltimateFutureUnity3DC#/Scripts/Scripts/InventoryController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> itemSpriteItem = new List<Sprite>();
+    [SerializeField] private List<int> countItemSprite = new List<int>();
+    public List<GameObject> PrefabObjectItem = new List<GameObject>();
+
+
+    public void SetItem(Sprite sprite,int count,GameObject prefabObject)
+    {
+        if (ChceckIsItemExistInEq(sprite))
+        {
+            countItemSprite[GetIndexCount(sprite)]+=count;
+            ToolBarController toolBarController = GetComponent<ToolBarController>();
+            toolBarController.SetSpriteAndCountToolBar(sprite, countItemSprite[GetIndexCount(sprite)]);
+        }
+        else
+        {
+            itemSpriteItem.Add(sprite);
+            PrefabObjectItem.Add(prefabObject);
+            countItemSprite.Add(count);
+            ToolBarController toolBarController = GetComponent<ToolBarController>();
+            toolBarController.SetSpriteAndCountToolBar(sprite, count);
+        }
+    }
+    private bool ChceckIsItemExistInEq(Sprite sprite)
+    {
+       return itemSpriteItem.Exists(x => x.name == sprite.name);
+    }
+    private int GetIndexCount(Sprite sprite)
+    {
+        return itemSpriteItem.IndexOf(sprite);
+    }
+}
---
+++ UltimateFutureUnity3DC#/Scripts/Scripts/PickUpMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpMaterial : MonoBehaviour
+{
+    [SerializeField] private HandController hanController=null;   
+    [SerializeField] private Sprite spritePickUpObject=null;
+    [SerializeField] private GameObject prefabPickUp = null;
+
+    [SerializeField] private bool isPistol = false;
+    [SerializeField] private bool isRifle = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            if (this.tag=="Gun")
+            {
+                InventoryController inventoryController = GameObject.Find("Canvas/InventoryPanel").GetComponent<InventoryController>();
+                inventoryController.SetItem(spritePickUpObject, 1, prefabPickUp);
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                InventoryController inventoryController = GameObject.Find("Canvas/InventoryPanel").GetComponent<InventoryController>();
+                inventoryController.SetItem(spritePickUpObject, 1, prefabPickUp);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
