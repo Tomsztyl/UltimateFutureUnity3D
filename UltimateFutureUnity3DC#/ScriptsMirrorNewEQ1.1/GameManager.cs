0fd659641ec5c30ff726601f548f13563ddb96f7
Tomsztyl <60610578+Tomsztyl@users.noreply.github.com>
2021-02-07 20:08:12 +0100

    Add files via upload

---
+++ UltimateFutureUnity3DC#/ScriptsMirrorNewEQ1.1/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Variables who check If Game is Active
+    [SerializeField] private bool isGameActive = false;
+    [SerializeField] private bool isInstantiate = false;
+    [SerializeField] private int howManyBoxCreate = 1;
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject boxPandora;
+
+    private void Update()
+    {
+        if (isGameActive==true)
+        {
+            InstantiatePlayerAndBox();
+        }
+        else
+        {
+            //Debug.Log("Game is not started");
+        }
+    }
+    private void InstantiatePlayerAndBox()
+    {
+        if (isInstantiate!=true)
+        {
+            return;
+        }
+        else
+        {
+            Instantiate(player, new Vector3(248.5316f, 0.7826424f, 246.4595f), Quaternion.identity);
+            for (int i = 0; i < howManyBoxCreate; i++)
+                Instantiate(boxPandora, new Vector3(Random.Range(0f, 248f), Random.Range(1.5f, 3f), Random.Range(0, 246f)), Quaternion.identity);
+            Debug.Log("Is Instant: "+ howManyBoxCreate +" box");
+            Screen.lockCursor = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            isInstantiate = false;
+        }
+
+    }
+}
---
+++ UltimateFutureUnity3DC#/ScriptsMirrorNewEQ1.1/PlayerMirrorController.cs
@@ -0,0 +1,188 @@
+﻿using Mirror;
+using Mirror.Examples.NetworkRoom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerMirrorController : NetworkBehaviour
+{
+    public bool isPlayer = false;
+    public PlayerController PlayerController;
+    [Header("This is HUD Player")]
+    public GameObject canvasHUD = null;
+    public Canvas CanvasHUD = null;
+    public UIManager UIManagerHUD = null;
+    public GameObject EventSystem = null;
+
+
+    [Header("This is a Equipment Player")]
+    public GameObject ToolBar = null;
+    public List<GameObject> SlorControllerToolBar = new List<GameObject>();
+    public GameObject EquipmentBox = null;
+    public List<GameObject> SlorControllerEquipmentBox = new List<GameObject>();
+    public GameObject InventoryManager = null;
+
+    [Header("Select Object Hand Player ")]
+    public GameObject rightHand;
+    public GameObject leftHand;
+
+
+
+
+
+    void OnValidate()
+    {
+        TriggerHudPlayer();
+        TriggerEquipmentPlayer();
+    }
+
+    void Start()
+    {
+        TriggerPlayerHUDStart();
+        FindAllSlotControllerServer();
+        isPlayer = isLocalPlayer;
+    }
+    private void Update()
+    {
+        if (isLocalPlayer) { StartChecker(); };
+    }
+
+
+    public void TriggerPlayerHUDStart()
+    {
+        PlayerController.enabled = isLocalPlayer;
+        CanvasHUD.enabled = isLocalPlayer;
+        UIManagerHUD.enabled = isLocalPlayer;
+        EventSystem.SetActive(isLocalPlayer);
+    }
+    public void TriggerHudPlayer()
+    {
+
+        if (PlayerController == null)
+            PlayerController = GetComponent<PlayerController>();
+
+        if (CanvasHUD == null)
+            CanvasHUD = GameObject.Find(this.name + "/Canvas").GetComponent<Canvas>();
+
+        if (UIManagerHUD == null)
+            UIManagerHUD = GameObject.Find(this.name + "/Canvas").GetComponent<UIManager>();
+
+        if (EventSystem == null)
+            EventSystem = GameObject.Find(this.name + "/Canvas/EventSystem").GetComponent<GameObject>();
+
+        if (canvasHUD == null)
+            canvasHUD = GameObject.Find(this.name + "/Canvas/EventSystem").GetComponent<GameObject>();
+
+    }
+    public void TriggerEquipmentPlayer()
+    {
+        if (ToolBar == null)
+            ToolBar = GameObject.Find(this.name + "/Canvas/EventSystem").GetComponent<GameObject>();
+
+        if (EquipmentBox == null)
+            EquipmentBox = GameObject.Find(this.name + "/Canvas/EventSystem").GetComponent<GameObject>();
+
+        if (InventoryManager == null)
+            InventoryManager = GameObject.Find(this.name + "/Canvas/EventSystem").GetComponent<GameObject>();
+    }
+
+
+
+    //Check Trigger Key
+    [Command]
+    public void FindAllSlotControllerServer()
+    {
+        FindAllSlotController();
+    }
+    [ClientRpc]
+    public void FindAllSlotController()
+    {
+        foreach (Transform child in ToolBar.transform)
+        {
+            SlorControllerToolBar.Add(child.gameObject);
+        }
+        foreach (Transform child in EquipmentBox.transform)
+        {
+            SlorControllerEquipmentBox.Add(child.gameObject);
+        }
+    }
+    public void StartChecker()
+    {
+        CheckTheTriggerKey();
+    }
+    public void CheckTheTriggerKey()
+    {
+        foreach(GameObject slotControllerObj in SlorControllerToolBar)
+        {
+            SlotController slotControllerCheck = slotControllerObj.GetComponent<SlotController>();
+            if (slotControllerCheck.ReturnKeyCodeObject()==KeyCode.None)
+            {
+                return;
+            }
+            else
+            {
+                if (Input.GetKeyDown(slotControllerCheck.ReturnKeyCodeObject()))
+                {
+                    foreach (var scriptableObject in ((NetworkRoomManagerExt)NetworkManager.singleton).scriptableObjectToMirror.Select(((value, index) => new { value, index })))
+                    {
+                        if (slotControllerCheck.ReturnPrefab() == null) { DestoryAllObjectInHandServer(); return; }
+                        else if (scriptableObject.value.name== slotControllerCheck.ReturnPrefab().name)
+                        {
+                            DestoryAllObjectInHandServer();
+                            SendSlotControllerToObjectServer(scriptableObject.index);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    [Command]
+    public void SendSlotControllerToObjectServer(int indexSpanObject)
+    {
+        SendSlotControllerToObjectClients(indexSpanObject);
+    }
+    [ClientRpc]
+    public void SendSlotControllerToObjectClients(int indexSpanObject)
+    {
+        var objectInstantiate = Instantiate(((NetworkRoomManagerExt)NetworkManager.singleton).objectHand, rightHand.transform);
+        NetworkServer.Spawn(objectInstantiate);
+
+
+        if (((NetworkRoomManagerExt)NetworkManager.singleton).scriptableObjectToMirror[indexSpanObject] is WeaponeObjectController)
+        {
+            WeaponeObjectController weaponeObjectController = (WeaponeObjectController)((NetworkRoomManagerExt)NetworkManager.singleton).scriptableObjectToMirror[indexSpanObject];
+            var objectWeaponeInstantiate = Instantiate(weaponeObjectController.ReturnWeaponeHand(), objectInstantiate.transform);
+            NetworkServer.Spawn(objectWeaponeInstantiate);
+            objectWeaponeInstantiate.transform.localPosition = weaponeObjectController.ReturnPositionInstantiate();
+            objectWeaponeInstantiate.transform.localEulerAngles = weaponeObjectController.ReturnRotationInstantiate();
+            objectWeaponeInstantiate.transform.localScale = weaponeObjectController.ReturnScaleInstantiate() ;
+        }
+        else if (((NetworkRoomManagerExt)NetworkManager.singleton).scriptableObjectToMirror[indexSpanObject] is HandObjectController)
+        {
+            HandObjectController handObjectController = (HandObjectController)((NetworkRoomManagerExt)NetworkManager.singleton).scriptableObjectToMirror[indexSpanObject];
+            var objectHandInstantiate = Instantiate(handObjectController.ReturnObjectInHandPref(), objectInstantiate.transform);
+            NetworkServer.Spawn(objectHandInstantiate);
+            objectHandInstantiate.transform.localPosition = handObjectController.ReturnPositionInstantiate();
+            objectHandInstantiate.transform.localEulerAngles = handObjectController.ReturnRotationInstantiate();
+            objectHandInstantiate.transform.localScale = handObjectController.ReturnScaleInstantiate();
+        }
+    }
+
+    [Command]
+    public void DestoryAllObjectInHandServer()
+    {
+        DestoryAllObjectInHandClients();
+    }
+    [ClientRpc]
+    public void DestoryAllObjectInHandClients()
+    {
+
+        if (rightHand.transform.Find("ObjectHand(Clone)") != null)
+        {
+            Debug.LogWarning("Is Exist");
+            Destroy(rightHand.transform.Find("ObjectHand(Clone)").gameObject);
+        }
+    }
+
+}
