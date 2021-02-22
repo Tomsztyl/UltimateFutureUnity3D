5b942836d5bfa27c70072475ea4e1ebd54b57a02
Tomsztyl <60610578+Tomsztyl@users.noreply.github.com>
2021-02-22 02:14:07 +0100

    Add files via upload

---
+++ ScriptsMirrorNewEQ1.3/AuthentionController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuthentionController : MonoBehaviour
+{
+    [Header("This is username display up to Player")]
+    [SerializeField] private Transform cameraChoose = null;
+
+}
